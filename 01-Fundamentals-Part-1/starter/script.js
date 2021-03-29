334d8cabcae728c88b70dea50265d6110fc34a96
lnbalon <lnbalon87@gmail.com>
2021-03-29 16:17:05 +0800

    edited the starter codes as part of lecture

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log('bong');
+console.log(23);
+
+let firstName = 'Leonard'
+let middleName = 'Navarro'
+let lastName = 'Balon'
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let Leonard = 4;
+let PI = 3.1415;
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Hacker';
+
+console.log(myCurrentJob)
+
