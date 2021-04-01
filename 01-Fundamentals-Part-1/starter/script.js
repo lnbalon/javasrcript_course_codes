e2078bdefe14ff94a63e7efe3ccbccaae3edb093
lnbalon <lnbalon87@gmail.com>
2021-04-02 04:01:10 +0800

    discovered the power of backticks

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -142,5 +142,15 @@ const currentYear = 2037
 const bong = "I'm " + firstName + ", a " + (currentYear - birthYear) + ' year old ' + job;
 console.log(bong)
 
-const bongNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}`
-console.log(bongNew)
+const bongNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}`;
+console.log(bongNew);
+
+console.log(`print this statement`);
+
+console.log('string with \n\
+multiple lines \n\
+lines')
+
+console.log(`string with 
+new
+lines`)
