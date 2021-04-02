fdaa754f0978f936d0fb1c2c088b6c8034cad603
lnbalon <lnbalon87@gmail.com>
2021-04-03 02:42:05 +0800

    added samples of truthy and falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -199,24 +199,30 @@ console.log(averageAge)
 
 // type conversion
 
-const inputYear = '1998';
-inputYear_num = Number(inputYear);
-console.log(inputYear_num + 18);
-console.log(inputYear + 18);
-console.log(typeof inputYear);
-console.log(Number('bong'));
-console.log(typeof NaN);
-console.log(String(23.12));
+// const inputYear = '1998';
+// inputYear_num = Number(inputYear);
+// console.log(inputYear_num + 18);
+// console.log(inputYear + 18);
+// console.log(typeof inputYear);
+// console.log(Number('bong'));
+// console.log(typeof NaN);
+// console.log(String(23.12));
 
-// type coercion
-console.log('I am ' + 23 + ' years old')
-console.log('23' - 12)
-console.log('23' * '2')
+// // type coercion
+// console.log('I am ' + 23 + ' years old')
+// console.log('23' - 12)
+// console.log('23' * '2')
 
-let n = '1' + 1;
-n--
-console.log(n)
-console.log(typeof n)
+// let n = '1' + 1;
+// n--
+// console.log(n)
+// console.log(typeof n)
 
 
 // Truthy and Falsy values
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(21));
+console.log(Boolean('bong'));
+console.log(Boolean(''));
+console.log(Boolean({}));
