cc63b7ce4bc5a31c1b1b579157ca24e31f624532
lnbalon <lnbalon87@gmail.com>
2021-04-03 02:48:33 +0800

    a

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -220,17 +220,28 @@ console.log(averageAge)
 
 
 // Truthy and Falsy values
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean(21));
-console.log(Boolean('bong'));
-console.log(Boolean(''));
-console.log(Boolean({}));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(21));
+// console.log(Boolean('bong'));
+// console.log(Boolean(''));
+// console.log(Boolean({}));
 
-const money = 0;
+// const money = 100;
 
-if (money) {
-    console.log(`Don't spend it all`)
-} else {
-    console.log(`get a fucking job`)
-}
+// if (money) {
+//     console.log(`Don't spend it all`)
+// } else {
+//     console.log(`get a fucking job`)
+// }
+
+// let height;
+// if (height) {
+//     console.log(`defined`)
+// } else {
+//     console.log(`define height`)
+// }
+
+const age = 18;
+
+if (age === 18) console.log('turned eighteen')
