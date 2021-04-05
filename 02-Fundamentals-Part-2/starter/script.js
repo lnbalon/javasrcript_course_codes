3c99e1f54823d7477006b9839e42d02138738280
lnbalon <lnbalon87@gmail.com>
2021-04-06 05:35:34 +0800

    chekpoint 7

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -17,3 +17,16 @@ console.log(fruitProcessor(10, 20));
 console.log(fruitProcessor(10, 202));
 console.log(fruitProcessor(1, 121));
 
+function isDivisibleByThree(num) {
+
+    const remainder = num % 3;
+    if (!remainder) {
+        return 'Divisible by 3!'
+    } else {
+        return 'Not divisible by 3!'
+    }
+}
+
+console.log(isDivisibleByThree(2));
+console.log(isDivisibleByThree(12));
+console.log(isDivisibleByThree(122));
