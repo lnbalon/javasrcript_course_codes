ce230d7769f824bc5aeb59a3fa2243c7a6bfaec7
lnbalon <lnbalon87@gmail.com>
2021-04-07 17:32:00 +0800

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,3 +57,16 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(2022);
 console.log(age2)
 
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(2022);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'bong'));
+console.log(yearsUntilRetirement(1211, 'd'));
