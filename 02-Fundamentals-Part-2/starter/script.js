ae2fa73c66b4a462e2b00c4392ac5d6f5b84d754
lnbalon <lnbalon87@gmail.com>
2021-04-10 18:18:47 +0800

    push and unshift

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,162 +1,170 @@
 'use strict';
 
-// introduction to function
-function logger() {
-    console.log('This is a logger message')
-}
+// // introduction to function
+// function logger() {
+//     console.log('This is a logger message')
+// }
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-// practice code 2
-function fruitProcessor(apples, oranges) {
-    // console.log(apples, oranges);
-    const juice = `juice with ${apples} apples and ${oranges} oranges`;
-    return juice
-}
+// // practice code 2
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//     return juice
+// }
 
-// different ways of showing the result to console
-const juiceResult = fruitProcessor(2, 3);
-console.log(juiceResult);
-console.log(fruitProcessor(10, 20));
-console.log(fruitProcessor(10, 202));
-console.log(fruitProcessor(1, 121));
+// // different ways of showing the result to console
+// const juiceResult = fruitProcessor(2, 3);
+// console.log(juiceResult);
+// console.log(fruitProcessor(10, 20));
+// console.log(fruitProcessor(10, 202));
+// console.log(fruitProcessor(1, 121));
 
-// practice code 3
-function isDivisibleByThree(num) {
-    // a function that tests if a number is divisible 
-    // by three or not. 
-    const remainder = num % 3;
-    if (!remainder) {
-        return 'Divisible by 3!'
-    } else {
-        return 'Not divisible by 3!'
-    }
-}
+// // practice code 3
+// function isDivisibleByThree(num) {
+//     // a function that tests if a number is divisible 
+//     // by three or not. 
+//     const remainder = num % 3;
+//     if (!remainder) {
+//         return 'Divisible by 3!'
+//     } else {
+//         return 'Not divisible by 3!'
+//     }
+// }
 
-console.log(isDivisibleByThree(2));
-console.log(isDivisibleByThree(12));
-console.log(isDivisibleByThree(122));
-
-
-// function decleration
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
-}
-
-const age1 = calcAge1(2022);
-console.log(age1);
+// console.log(isDivisibleByThree(2));
+// console.log(isDivisibleByThree(12));
+// console.log(isDivisibleByThree(122));
 
 
-// function expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
+// // function decleration
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
 
-const age2 = calcAge2(2022);
-console.log(age2)
-
-// Arrow function
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(2022);
-console.log(age3);
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    return `${firstName} retires in ${retirement} years`;
-}
-
-console.log(yearsUntilRetirement(1991, 'bong'));
-console.log(yearsUntilRetirement(1211, 'd'));
+// const age1 = calcAge1(2022);
+// console.log(age1);
 
 
-// functions calling other functions
-function cutFruitPieces(fruit) {
-    return fruit * 4;
-}
+// // function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
 
-function fruitProcessor2(apples, oranges) {
-    // console.log(apples, oranges);
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-    const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
-    return juice;
-}
+// const age2 = calcAge2(2022);
+// console.log(age2)
 
-console.log(fruitProcessor(2, 3));
-console.log(fruitProcessor2(2, 3));
+// // Arrow function
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(2022);
+// console.log(age3);
 
-// Coding exercise # 1
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
 
-// arrow function to get the average
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// console.log(yearsUntilRetirement(1991, 'bong'));
+// console.log(yearsUntilRetirement(1211, 'd'));
 
-const dolphinsAverage1 = calcAverage(44, 23, 71);
-const koalasAverage1 = calcAverage(65, 54, 49);
 
-const dolphinsAverage2 = calcAverage(85, 54, 41);
-const koalasAverage2 = calcAverage(23, 34, 27);
+// // functions calling other functions
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
 
-// function to get the winner
-function checkWinner(avgKoalas, avgDolphins) {
+// function fruitProcessor2(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+//     const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
+//     return juice;
+// }
 
-    // console.log(avgKoalas, avgDolphins)
-    if (avgDolphins >= avgKoalas * 2) {
-        console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
-    } else if (avgKoalas >= avgDolphins * 2) {
-        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
-    } else {
-        console.log('no one wins!')
-    }
-}
+// console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor2(2, 3));
 
-// test the functions
-checkWinner(koalasAverage1, dolphinsAverage1);
-checkWinner(koalasAverage2, dolphinsAverage2);
-checkWinner(440, 202);
+// // Coding exercise # 1
 
-// arrays
-const name1 = 'bong';
-const name2 = 'nong';
-const name3 = 'maan';
-const name4 = 'godo';
+// // arrow function to get the average
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// literal syntax
-const names = ['bong', 'nong', 'maan', 'godo'];
-console.log(names)
+// const dolphinsAverage1 = calcAverage(44, 23, 71);
+// const koalasAverage1 = calcAverage(65, 54, 49);
 
-const years = new Array(1991, 1983, 2000, 4000);
+// const dolphinsAverage2 = calcAverage(85, 54, 41);
+// const koalasAverage2 = calcAverage(23, 34, 27);
 
-// accessing arrays
-console.log(names[0])
+// // function to get the winner
+// function checkWinner(avgKoalas, avgDolphins) {
 
-// length arrays
-console.log(names.length)
+//     // console.log(avgKoalas, avgDolphins)
+//     if (avgDolphins >= avgKoalas * 2) {
+//         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+//     } else if (avgKoalas >= avgDolphins * 2) {
+//         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+//     } else {
+//         console.log('no one wins!')
+//     }
+// }
 
-// access the last element
-console.log(names[names.length - 1])
+// // test the functions
+// checkWinner(koalasAverage1, dolphinsAverage1);
+// checkWinner(koalasAverage2, dolphinsAverage2);
+// checkWinner(440, 202);
 
-// array mutation
-names[2] = 'Maan';
-console.log(names);
+// // arrays
+// const name1 = 'bong';
+// const name2 = 'nong';
+// const name3 = 'maan';
+// const name4 = 'godo';
 
-// array not mutable; results in error
-// names = ['new', 'new_name'];
+// // literal syntax
+// const names = ['bong', 'nong', 'maan', 'godo'];
+// console.log(names)
 
-// more array operations
-const bong = ['Balon', 'UPLB', 2021 - 1987, names[0]];
-console.log(bong)
+// const years = new Array(1991, 1983, 2000, 4000);
 
-// accessing arrays
-const birthYears = [1990, 1967, 2002, 2010, , 2018];
+// // accessing arrays
+// console.log(names[0])
 
-console.log(calcAge1(birthYears[2]))
-console.log(calcAge1(birthYears[1]))
-console.log(calcAge1(birthYears[3]))
+// // length arrays
+// console.log(names.length)
 
-const ages = [calcAge1(birthYears[3]), calcAge1(birthYears[2])]
-console.log(ages)
+// // access the last element
+// console.log(names[names.length - 1])
+
+// // array mutation
+// names[2] = 'Maan';
+// console.log(names);
+
+// // array not mutable; results in error
+// // names = ['new', 'new_name'];
+
+// // more array operations
+// const bong = ['Balon', 'UPLB', 2021 - 1987, names[0]];
+// console.log(bong)
+
+// // accessing arrays
+// const birthYears = [1990, 1967, 2002, 2010, , 2018];
+
+// console.log(calcAge1(birthYears[2]))
+// console.log(calcAge1(birthYears[1]))
+// console.log(calcAge1(birthYears[3]))
+
+// const ages = [calcAge1(birthYears[3]), calcAge1(birthYears[2])]
+// console.log(ages)
 
 // methods on arrays
+const numList = [1, 2, 3, 4, 5];
+const lenNumList = numList.push(6)
+
+console.log(numList)
+console.log(lenNumList)
+
+numList.unshift(0)
+console.log(numList)
