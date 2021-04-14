64fdaf0fe3b1c004cc2c42255e292d1eee0d013d
lnbalon <lnbalon87@gmail.com>
2021-04-14 08:05:17 +0800

    while loop done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -391,3 +391,17 @@
 
 // console.log(first)
 
+// while loop
+
+let i = 0;
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+
+// while loop rolling a dice
+let dice = Math.trunc(Math.random(0, 1) * 6 + 1);
+while (dice != 6) {
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random(0, 1) * 6 + 1);
+}
