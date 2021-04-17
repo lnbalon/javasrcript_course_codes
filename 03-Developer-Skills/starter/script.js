8cbeab6e5ec4fa47bad1d2365c2e53150d1b933b
lnbalon <lnbalon87@gmail.com>
2021-04-17 22:48:00 +0800

    changed to single quote

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
 const x = 10;
 
 const y = 2;
 if (x === 10) {
-  console.log("good");
+  console.log('good');
 }
