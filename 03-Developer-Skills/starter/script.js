2dba78d1a9f56572835f9308626d76cbc0c288d9
lnbalon <lnbalon87@gmail.com>
2021-04-21 10:33:19 +0800

    new problem

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,14 +1,4 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 10;
-
-const y = 2;
-if (x === 10) {
-  console.log('good');
-}
-
-const calcAge = birthYear => 2021 - birthYear;
-
-console.log('bong');
-console.log('three');
+/
