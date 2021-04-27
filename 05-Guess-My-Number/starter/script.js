c6f44c22e44f56eaa4df6f5efe70184ff40418c8
lnbalon <lnbalon87@gmail.com>
2021-04-28 07:39:07 +0800

    handling click values

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,3 +3,10 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 200;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
