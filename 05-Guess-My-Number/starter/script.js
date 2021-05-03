0281124d29b662a0fa39a9716243741fc2eb87bd
lnbalon <lnbalon87@gmail.com>
2021-05-03 16:06:24 +0800

    checkpoint

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,24 @@
 'use strict';
 
+// start the game logic
+
+// define a number from one to twenty
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
-
-// start the game logic
